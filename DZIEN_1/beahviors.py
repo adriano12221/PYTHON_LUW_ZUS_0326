905bbe6b3e29c6d22973f9499e5c6c6f1467d95b
albim72 <marcin.albiniak72@gmail.com>
2026-03-02 12:02:51 +0100

    Add events list for competition event stream

---
+++ DZIEN_1/beahviors.py
@@ -0,0 +1,12 @@
+#strumień zdarzeń z zawodów
+
+events = [
+    (5,101,"overtake"),
+    (7,102,"overtake"),
+    (9,101,"gel"),
+    (12,101,"overtake"),
+    (14,102,"gel"),
+    (16,101,"overtake"),
+    (18,101,"overtake"),
+    (21,102,"gel"),
+]
